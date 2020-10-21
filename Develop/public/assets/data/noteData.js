7989bcf2bdcba240cf6560df76bc0fcdf3f90004
JuTo_Hub <JustinTorres1@Outlook.com>
2020-10-21 18:17:15 -0500

    Added noteData.js file, added express functionality

---
+++ Develop/public/assets/data/noteData.js
@@ -0,0 +1,9 @@
+const noteArray = [
+  {
+    noteTitle: "Ahmed",
+    noteText: "ahmed@example.com",
+    noteID: "afhaque89",
+  }
+];
+
+module.exports = noteArray;
--- Develop/public/assets/routes/apiRoutes.js
+++ Develop/public/assets/routes/apiRoutes.js
@@ -1,12 +1,10 @@
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// These data sources hold arrays of information on note-data.
 // ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
-
+var noteData = require("../data/noteData");
 
 // ===============================================================================
 // ROUTING
@@ -19,45 +17,36 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
-  });
-
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
+  app.get("/api/notes", function(req, res) {
+    res.json(noteData);
   });
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
+  // (ex. User fills out a note section... this data is then sent to the server...
+  // Then the server saves the data to the noteData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
+  app.post("/api/notes", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
+    if (noteData.length < 5) {
+      noteData.push(req.body);
       res.json(true);
     }
     else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
+         }
   });
 
   // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+  // Used to clear the noteData array. Possibly unnecessary.
 
   app.post("/api/clear", function(req, res) {
     // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
-
+    noteData.length = 0;
     res.json({ ok: true });
   });
 };
